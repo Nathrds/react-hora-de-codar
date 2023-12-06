cfabb38c69419655fde6944534f512751099128a
Nathália <nathaliarsantos@live.com>
2023-12-06 16:07:40 -0300

    tipos de comentários

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,9 @@
+// arquivo de estilo
 const FirstComponent = () => {
+    // algum comentário
     return (
         <div>
+            {/* tem que ser dentro de algum elemento pai para comentar*/}
             <h1>Meu primeiro componente</h1>
             <p className="test">Meu texto</p>
         </div>
