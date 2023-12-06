02608264780593736cdc617fc4dfe8b762de4b14
Nathália <nathaliarsantos@live.com>
2023-12-06 14:59:54 -0300

    desafio 01 hora de codar

---
+++ 1_INTRO/hello-world/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <h2>Emmet funcinou</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </div>
+  );
+}
+
+export default App;
