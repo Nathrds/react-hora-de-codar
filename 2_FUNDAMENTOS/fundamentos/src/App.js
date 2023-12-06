265a186926f9036c177094282c9abaf668ff96eb
Nathália <nathaliarsantos@live.com>
2023-12-06 16:18:57 -0300

    template expressions

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,5 +1,6 @@
 // components
 import FirstComponent from './components/FirstComponent';
+import TemplateExpressions from './components/TemplateExpressions';
 
 
 // style - CSS
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <h1>Fundamentos React</h1>
       <FirstComponent />
+      <TemplateExpressions />
     </div>
   );
 }
