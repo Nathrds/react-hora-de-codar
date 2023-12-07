aacb8e9177c84f75a416c56f0f55a247d3bd7d39
Nathália <nathaliarsantos@live.com>
2023-12-07 16:02:08 -0300

    imagens

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; não é necessária, pode apagar
 import './App.css';
+
+import City from './assets/city.jpg'; // importando a imagem como componentes, se mudar terá que alterar só em um lugar
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+        {/* quando a imagem está em Public posso acessar ela diretamente, sem fazer caminho de pastas */}
+      </div>
+      {/* Imagem em assets */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </div>
   );
 }
