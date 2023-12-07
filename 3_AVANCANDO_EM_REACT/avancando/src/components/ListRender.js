ccf1777a642cdc1660de82bae0de536422001c58
Nathália <nathaliarsantos@live.com>
2023-12-07 16:54:55 -0300

    propriedade key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -3,11 +3,25 @@ import {useState} from 'react';
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
 
+    // mais recomendado é esse por se necessitar de alteração é mais fácil
+    const [user] = useState([
+        { id: 1, name: "Ana", age: 31 },
+        { id: 724565, name: "João", age: 28},
+        { id: 832156, name: "Clara", age: 44}
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {user.map((user) => (
+                <li key={user.id}>
+                    {user.name} - {user.age}
+                </li>
             ))}
         </ul>
     </div>
