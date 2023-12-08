dcf5b98c6a1e3cff586a1be7ffcc7ed77ab8a657
Nathália <nathaliarsantos@live.com>
2023-12-08 11:08:53 -0300

    introdução a props

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg'; não é necessária, pode apagar
+import { useState } from 'react';
 import './App.css';
 
 import City from './assets/city.jpg'; // importando a imagem como componentes, se mudar terá que alterar só em um lugar
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const name2 = "Joaquim";
+
+  const [userName] = useState('Maria');
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -22,6 +28,7 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name={userName} />
     </div>
   );
 }
