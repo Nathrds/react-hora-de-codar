70ad0571175f57d76894f4387696a5c470d2e421
Nathália <nathaliarsantos@live.com>
2023-12-11 19:19:49 -0300

    consumindo states

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -26,6 +26,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3); // tentativas que você quer que o usuário tenha
+  const [score, setScore] = useState(0); // pontuação 
+
   const pickWordAndCategory = () => {
     // pick a random category
     const categories = Object.keys(words)
@@ -57,7 +62,7 @@ function App() {
     // fill states
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   }
@@ -75,7 +80,7 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} pickedWord={pickedWord} pickedCategory={pickedCategory} letters={letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} guesses={guesses} score={score} />}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
