dafe04ca017836d5e6313ca3792d09fa988c4233
Nathália <nathaliarsantos@live.com>
2023-12-11 19:37:02 -0300

    evento inserção de letra

--- 6_SECRET_WORD/secretword/src/components/Game.js
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -1,7 +1,21 @@
+import { useState, useRef } from 'react';
 import './Game.css';
 
 const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
-  return (
+    const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        verifyLetter(letter);
+
+        setLetter("");
+
+        letterInputRef.current.focus(); // focar nesse elemento após o fim do submit - continuar focando
+    }
+  
+    return (
     <div className='game'>
         <p className='points'>
             <span>Pontuação: {score}</span>
@@ -22,8 +36,8 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
         </div>
         <div className="letterContainer">
             <p>Tente adivinhar uma letra da palavra:</p>
-            <form>
-                <input type="text" name='letter' maxLength="1" required />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='letter' maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef} />
                 <button>Jogar!</button>
             </form>
         </div>
@@ -31,7 +45,7 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
             <p>Letras já utilizadas:</p>
             {wrongLetters.map((letter, i) => (
                 <span key={i}>{letter}, </span>
-            ))}
+            ))} 
         </div>
     </div>
   )
