3e0d52f94ef6caf5686f2b2baa4fd166644ba3f9
Nathália <nathaliarsantos@live.com>
2023-12-11 16:17:10 -0300

    select no React

--- 5_FORM_EM_REACT/form/src/App.js
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm user={{name: "Ana", email: "ana@gmail.com"}} />
+      <MyForm user={{name: "Ana", email: "ana@gmail.com", bio: "Sou Arquiteta", role: 'admin'}} />
     </div>
   );
 }
--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -8,7 +8,9 @@ const MyForm = ({user}) => {
   const [name, setName] = useState(user ? user.name : '');
   const [email, setEmail] = useState(user ? user.email : '');
 
-  const [bio, setBio] = useState("");
+  const [bio, setBio] = useState(user ? user.bio : '');
+
+  const [role, setRole] = useState(user ? user.role : '');
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -20,7 +22,7 @@ const MyForm = ({user}) => {
   const handleSubmit = (event) => {
     event.preventDefault(); // parar o envio no botão o formulário não recarregar a página
     console.log('enviando o formulário');
-    console.log(name, email, bio);
+    console.log(name, email, bio, role);
 
     // validação
     // envio
@@ -50,6 +52,15 @@ const MyForm = ({user}) => {
             <label>
               <span>Bio:</span>
               <textarea name="bio" placeholder='Descrição do usuário' onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+            </label>
+            {/* 9- select */}
+            <label>
+              <span>Função no sistema</span>
+              <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+                <option value="user">Usuário</option>
+                <option value="editor">Editor</option>
+                <option value="admin">Administrador</option>
+              </select>
             </label>
             <input type="submit" value="Enviar" />
         </form>
