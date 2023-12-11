e446f106c46f07c78677cf9d3870c15f07f4de6e
Nathália <nathaliarsantos@live.com>
2023-12-11 16:02:59 -0300

    resetando formulários

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -19,6 +19,13 @@ const MyForm = ({user}) => {
     event.preventDefault(); // parar o envio no botão o formulário não recarregar a página
     console.log('enviando o formulário');
     console.log(name, email);
+
+    // validação
+    // envio
+
+    // 7- limpar form (resetando)
+    setName("");
+    setEmail("");
   };
 
   return (
