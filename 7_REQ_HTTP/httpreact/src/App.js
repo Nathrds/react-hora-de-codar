bd778f82e03ae12511847381d342ef7b85ebf43b
Nathália <nathaliarsantos@live.com>
2023-12-12 15:34:09 -0300

    resgatando dados da API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 
+import {useState, useEffect} from 'react';
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  // salvar dados. O primeiro salva o segundo alxilia a colocar os produtos em algum lugar
+  const [products, setProducts] = useState([]);
+
+  // 1- resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
