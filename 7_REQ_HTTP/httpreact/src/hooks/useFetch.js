a2b8c6d3715f7a2d3c37969ca2d3a0759e9857d3
Nathália <nathaliarsantos@live.com>
2023-12-12 17:34:35 -0300

    refatorando o POST

--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -4,6 +4,24 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
    const [data, setData] = useState(null); 
 
+   // 5 - refatorando post
+   const [config, setConfig] = useState(null); // configurar o método que vai ser configurado [post, cabeçalhos]
+   const [method, setMethod] = useState(null); // vai dizer qual method vai estar utilizando [get ou post]
+   const [callFetch, setCallFetch] = useState(false); // tazer dados atualizados
+
+   const httpConfig = (data, method) => {
+    if(method === "POST") {
+        setConfig({
+            method,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data),
+        });
+        setMethod(method);
+    }
+   };
+
    useEffect(() => {
     const fetchData =async () => {
         const res = await fetch(url)
@@ -13,7 +31,23 @@ export const useFetch = (url) => {
         setData(json)
     }
     fetchData();
-   }, [url]);
+   }, [url, callFetch]);
 
-   return {data};
+   // 5 - refatorando post
+   useEffect(() => {
+    const httpRequest = async () => {
+        if(method === 'POST') {
+            let fetchOptions = [url, config]
+    
+            const res = await fetch(...fetchOptions);
+    
+            const json = await res.json();
+    
+            setCallFetch(json);
+        }
+    }
+    httpRequest();
+   }, [config, method, url]);
+
+   return {data, httpConfig};
 };
