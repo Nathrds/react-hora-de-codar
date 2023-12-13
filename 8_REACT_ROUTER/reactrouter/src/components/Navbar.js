2225861f7b0e1d3beccb2c0ced6398bd3a8a70b7
Nathália <nathaliarsantos@live.com>
2023-12-13 17:32:53 -0300

    navlink - link ativo

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -1,14 +1,18 @@
-// 2 - links com React Router
 import './Navbar.css';
-
-import { Link } from 'react-router-dom';
+// 2 - links com React Router
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
         {/* faz o rediricionamento trocando componente - não da o reloading e sim troca o componente*/}
-        <Link to="/">Home</Link> 
-        <Link to="/about">Sobre</Link>
+        {/* <Link to="/">Home</Link> 
+        <Link to="/about">Sobre</Link> */}
+        {/* navlink deixa o link ativo para o link que estiver no nomento - cores / className outro modo */}
+        <NavLink to="/" /* className={({isActive}) => (isActive ? "esta-ativo" : "nao-ativo")} tem que repetir em todos os navlinks*/>
+          Home
+        </NavLink>
+        <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 };
