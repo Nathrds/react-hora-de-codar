8d0c7ae88e158789fee31356d8a160173b1dcbc6
Nathália <nathaliarsantos@live.com>
2023-12-13 16:55:45 -0300

    rotas dinâmicas

---
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -0,0 +1,14 @@
+import {useParams} from 'react-router-dom';
+
+const Product = () => {
+    // 4 - rota dinamica
+    const {id} = useParams();
+
+  return (
+    <div>
+        <p>ID do produto: {id}</p>
+    </div>
+  )
+}
+
+export default Product
