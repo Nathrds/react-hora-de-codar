a113eee5e7c66bba4611cd991a8f6bcbf35e0178
Nathália <nathaliarsantos@live.com>
2023-12-13 16:29:26 -0300

    Link - navegação entre páginas

---
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+        {/* faz o rediricionamento trocando componente - não da o reloading e sim troca o componente*/}
+        <Link to="/">Home</Link> 
+        <Link to="/about">Sobre</Link>
+    </nav>
+  )
+};
+
+export default Navbar;
