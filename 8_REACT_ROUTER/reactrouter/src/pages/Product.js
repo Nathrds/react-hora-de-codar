a81d2e945a49bda22750970af3f8cc7ca4949dd3
Nathália <nathaliarsantos@live.com>
2023-12-13 17:03:37 -0300

    carregamento dinâmico de dados

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -1,12 +1,25 @@
 import {useParams} from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 const Product = () => {
     // 4 - rota dinamica
     const {id} = useParams();
 
+    // 5 - carregamento de dado individual
+    const url = "http://localhost:3000/products/" + id;
+    const {data: product, loading, error} = useFetch(url);
+
   return (
     <div>
         <p>ID do produto: {id}</p>
+        {error && <p>Ocorreu um erro...</p>}
+        {loading && <p>Carregando...</p>}
+        {product && (
+            <div>
+                <h1>{product.name}</h1>
+                <p>R${product.price}</p>
+            </div>
+        )}
     </div>
   )
 }
