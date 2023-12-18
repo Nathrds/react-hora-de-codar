f9c615aa7ff0481783aa91903b10ee55161ae19f
Nathália <nathaliarsantos@live.com>
2023-12-18 17:22:26 -0300

    login usuário
    estruturando página de login

--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.jsx
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.jsx
@@ -81,7 +81,7 @@ export const useAuthentication = () => {
 
         try {
             await signInWithEmailAndPassword(auth, data.email, data.password);
-
+            setLoading(false);
         } catch (error) {
             console.log(error.message);
             console.log(typeof error.message);
