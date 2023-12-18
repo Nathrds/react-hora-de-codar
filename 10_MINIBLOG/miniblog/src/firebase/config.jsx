6bec24eea779773c26dac317ee742325ed6c3211
Nathália <nathaliarsantos@live.com>
2023-12-18 15:36:58 -0300

    registro do usuário no sistema
    firebase

--- 10_MINIBLOG/miniblog/src/firebase/config.jsx
+++ 10_MINIBLOG/miniblog/src/firebase/config.jsx
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firebase";
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAlDtYVFM2dN7OLwnCwZexRWqUuO27r7yE",
