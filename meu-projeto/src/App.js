a4784d32aa5eb4cfacd1e35d87cd61ae67b53185
Nathália <nathaliarsantos@live.com>
2024-06-27 15:09:00 -0300

    componente 'HelloWord'

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// componentes
+import { HelloWord } from './components/HelloWord';
+
+
 function App() {
+  const name = 'Nathalia'
+
+  const newName = name.toUpperCase()
+
+  function sum (a, b) {
+    return a + b
+  }
+
+  // tamanho no final de 150 x 150 
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="minha imagem" />
+      <HelloWord />
     </div>
   );
 }
---
+++ meu-projeto/src/components/HelloWord.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+export const HelloWord = () => {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>    
+    </div>
+  )
+}
